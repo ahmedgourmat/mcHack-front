c603faa55b6d370983c7c36674b0dbab30f08e85
Salah <ls_nacef@esi.dz>
2024-04-26 07:51:23 +0100

    side bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import {Routes , Route} from 'react-router-dom'
-import Signup from './Pages/Signup'
+import { Routes, Route } from "react-router-dom"
+import Signup from "./Pages/Signup"
+import Home from "./Pages/Home"
 
 function App() {
-
-
   return (
     <div>
       <Routes>
-        <Route path='/signup' element={<Signup/>} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </div>
   )
