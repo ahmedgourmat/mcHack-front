fa7b0383adeb6d2c193547e9867a9773d9a054da
ahmed <grmidou15@gmail.com>
2024-04-26 07:00:12 +0100

    structure push

---
+++ src/Components/AuthSideBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AuthSideBar() {
+  return (
+    <div>
+      aa
+    </div>
+  )
+}
+
+export default AuthSideBar
---
+++ src/Pages/Signup.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Box} from '@chakra-ui/react'
+import AuthSideBar from '../Components/AuthSideBar'
+
+function Signup() {
+  return (
+    <Box>
+        <AuthSideBar/>
+    </Box>
+  )
+}
+
+export default Signup
