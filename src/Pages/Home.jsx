38c28032c7b92f9436cdc63d7445dae33fb598ef
Salah <ls_nacef@esi.dz>
2024-04-26 10:31:19 +0100

    adds

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,15 +1,30 @@
 import React from "react"
 import HomeSideBar from "../Components/HomeSideBar"
 import { Box } from "@chakra-ui/react"
+import CardBox from "../Components/CardBox"
+import ScheduleBox from "../Components/ScheduleBox"
+import NewSessionButton from "../Components/NewSessionButton"
 
 const Home = () => {
   return (
-    <div>
-      <Box display="flex">
+    <div style={{width:'100%'}}>
+      <Box display="flex" width='100%'>
         <Box>
           <HomeSideBar />
         </Box>
-        <Box></Box>
+        <Box width='100%'>
+          <CardBox
+            title="Title"
+            text="Lorem ipsum dolor sit amet"
+            date="April 30, 2024"
+          />
+          <ScheduleBox
+            session="Title"
+            title="Lorem ipsum dolor sit amet"
+            text="ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssApril 30, 2024"
+          />
+          <NewSessionButton />
+        </Box>
       </Box>
     </div>
   )
