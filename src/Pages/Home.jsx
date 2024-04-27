31997d63ab65024db9dfb09d90992b51c285587d
ahmed <grmidou15@gmail.com>
2024-04-27 06:31:16 +0100

    wow

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,17 +1,11 @@
-<<<<<<< HEAD
 import React from "react"
 import HomeSideBar from "../Components/HomeSideBar"
 import CardBox from "../Components/CardBox"
 import ScheduleBox from "../Components/ScheduleBox"
 import NewSessionButton from "../Components/NewSessionButton"
-=======
-import { Box } from '@chakra-ui/react'
-import React from 'react'
->>>>>>> 8eb7a6dfbeb2500a2b89a441a50b9ff9bacbdaa8
 
 function Home() {
   return (
-<<<<<<< HEAD
     <div>
       <div className="flex">
         <div>
@@ -69,11 +63,6 @@ function Home() {
         </div>
       </div>
     </div>
-=======
-    <Box>
-      
-    </Box>
->>>>>>> 8eb7a6dfbeb2500a2b89a441a50b9ff9bacbdaa8
   )
 }
 
